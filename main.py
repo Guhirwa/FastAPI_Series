e89119f84e9bf49d68a1d52e6ae3e62c92d525d2
Guhirwa <christianguhirwa74@gmail.com>
2026-05-07 11:03:24 +0200

    refactor(main): use band object and refactor the retuned list
    - import shcema and move enum in the schema file
    - use Band type hint instead of dict for enforcing type safety
    - return the list of bands with verified approach

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI, HTTPException
-from enum import Enum
+from schema import GenreURLChoices, Band
 
 app = FastAPI()
-
-class GenreURLChoices(Enum):
-    Rock = "rock"
-    Electronic = "electronic"
-    Metal = "metal"
-    Hip_Hop = "hip-hop"
 
 BANDS = [
     {"id":1, "name": "The Kinks", "genre": "Rock"},
@@ -17,8 +11,10 @@ BANDS = [
 ]
 
 @app.get("/bands")
-async def bands() -> list[dict]:
-    return BANDS
+async def bands() -> list[Band]:
+    return [
+        Band(**b) for b in BANDS
+    ]
 
 @app.get("/bands/{band_id}", status_code=200)
 async def band(band_id: int) -> dict:
