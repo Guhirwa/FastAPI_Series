c23eed540b5c287b037d5b6a4fd5d674e8375071
Guhirwa <christianguhirwa74@gmail.com>
2026-05-07 09:27:09 +0200

    chore(error): handler error using HTTPException
    - import HTTPException from fastapi package
    - raise the exception whenever the user search for non existenced band

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -13,6 +13,15 @@ BANDS = [
 async def bands() -> list[dict]:
     return BANDS
 
-@app.get("/about")
-async def about() -> str:
-    return "An Exceptional Company"
+@app.get("/bands/{band_id}")
+async def band(band_id: int) -> dict:
+
+    # for existing_band in BANDS:
+    #     if existing_band["id"] == band_id:
+    #         return existing_band
+    # raise HTTPException(status_code=404, detail="Band not found")
+
+    existing_band = next((b for b in BANDS if b["id"] == band_id), None)
+    if existing_band is None:
+        raise HTTPException(status_code=404, detail="Band not found")
+    return existing_band
