7a1d4d45b79e9761e875f6f69805cfc1b032185e
Guhirwa <christianguhirwa74@gmail.com>
2026-05-11 15:34:11 +0200

    feat(main): add the optional ability to all GET Bands endpoints
    - add ability to accept a query parameter an dreturn the list of those match with the genre provided in the query parameter
    - make that functionality so that whenever the query parameter is not provided it will receive None as default and then retun all Bands
    - use union type hint to match enum or None

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ BANDS = [
 ]
 
 @app.get("/bands")
-async def bands() -> list[Band]:
+async def bands(genre: GenreURLChoices | None = None) -> list[Band]:
+    if genre:
+        return [
+            Band(**b) for b in BANDS if b["genre"].lower() == genre.value
+        ]
     return [
         Band(**b) for b in BANDS
     ]
